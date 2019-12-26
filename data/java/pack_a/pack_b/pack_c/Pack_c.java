910994100dca4156c962a80ace4881618c1c2bdb
me <7423116+fredjb124@users.noreply.github.com>
2019-12-26 13:37:54 -0500

    Add files via upload

---
+++ data/java/pack_a/pack_b/pack_c/Pack_c.java
@@ -0,0 +1,17 @@
+package data.java.pack_a.pack_b.pack_c;
+
+public class Pack_c {
+	private static String pri_stat = "pri_stat";
+	static String def_stat = "def_stat";
+	protected static String pro_stat = "pro_stat";
+	public static String pub_stat = "pub_stat";
+
+	private String pri_non_stat = "pri_non_stat";
+	protected String pro_non_stat = "pro_non_stat";
+	String def_non_stat = "def_non_stat";
+	public String pub_non_stat = "pub_non_stat";
+
+	public static void main(String args[]) {
+	System.out.println("In Pack_c...");
+	}
+}//end class
