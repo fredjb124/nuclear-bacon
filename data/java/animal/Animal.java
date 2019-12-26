d3a2deddadde72eae90cc7dbb50459d6b46cc0cf
me <7423116+fredjb124@users.noreply.github.com>
2019-12-26 12:17:49 -0500

    Add files via upload

---
+++ data/java/animal/Animal.java
@@ -0,0 +1,23 @@
+package data.java.animal;
+import data.java.animal.dog.Dog;
+
+public class Animal {
+	private String animalString;
+
+	public String getAnimalString() {
+		return animalString;
+	}
+
+	public void setAnimalString(String animalString) {
+		this.animalString = animalString;
+	}
+
+	public Animal (String animalString) {
+		this.animalString = animalString;
+	}
+
+    public static void main(String[] args) {
+		Animal animal_01 = new Animal("Animal_01");
+		Dog dog_01 = new Dog("Dog_01");
+	}
+}
