86e743a6f38512e6b9ff23ce593ed8a9514235ed
me <7423116+fredjb124@users.noreply.github.com>
2019-12-26 13:33:32 -0500

    Add files via upload

---
+++ data/java/food/fruit/apple/Apple.java
@@ -0,0 +1,20 @@
+package data.java.food.fruit.apple;
+import data.java.food.fruit.Fruit;
+
+public class Apple extends Fruit {
+//class Apple extends food.fruit.Fruit {
+	private static String pri_stat = "pri_stat";
+	static String def_stat = "def_stat";
+	protected static String pro_stat = "pro_stat";
+	public static String pub_stat = "pub_stat";
+
+	private String pri_non_stat = "pri_non_stat";
+	protected String pro_non_stat = "pro_non_stat";
+	String def_non_stat = "def_non_stat";
+	public String pub_non_stat = "pub_non_stat";
+
+	public static void main(String args[]) {
+	//System.out.println("In data.java.food.fruit.apple.Apple...fruit str = "+fruit_str);
+	//System.out.println("In data.java.food.fruit.apple.Apple..."+apple_str);
+	}
+}//end class
