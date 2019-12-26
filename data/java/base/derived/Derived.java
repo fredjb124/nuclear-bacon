fd9c141242921b61451d7374112ad6d4b6de2253
me <7423116+fredjb124@users.noreply.github.com>
2019-12-26 13:19:51 -0500

    Add files via upload

---
+++ data/java/base/derived/Derived.java
@@ -0,0 +1,21 @@
+package data.java.base.derived;
+import data.java.base.Base;
+
+class Derived extends Base{ 
+	private static String pri_stat = "pri_stat";
+	static String def_stat = "def_stat";
+	protected static String pro_stat = "pro_stat";
+	public static String pub_stat = "pub_stat";
+
+	private String pri_non_stat = "pri_non_stat";
+	protected String pro_non_stat = "pro_non_stat";
+	String def_non_stat = "def_non_stat";
+	public String pub_non_stat = "pub_non_stat";
+
+	public static void main(String args[]) {
+		//System.out.println("In Derived..."+derived_str);
+		
+	}//end main
+
+
+}//end class
